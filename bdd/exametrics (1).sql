ea854dbb3da8a5e0b0ee3a5884050563abf16ac9
Beehaku <remy.salles@imerir.com>
2016-05-19 09:26:52 +0200

    bdd

--- bdd/exametrics (1).sql
+++ bdd/exametrics (1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 18 Mai 2016 à 14:31
+-- Généré le :  Jeu 19 Mai 2016 à 09:25
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -23,17 +23,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `area`
+--
+
+CREATE TABLE IF NOT EXISTS `area` (
+  `idArea` int(11) NOT NULL AUTO_INCREMENT,
+  `nomArea` varchar(45) NOT NULL,
+  `colorArea` varchar(45) NOT NULL,
+  PRIMARY KEY (`idArea`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `note`
 --
 
 CREATE TABLE IF NOT EXISTS `note` (
   `idNote` int(11) NOT NULL,
-  `auteurNote` varchar(45) NOT NULL,
-  `texteNote` text NOT NULL,
+  `authorNote` varchar(45) NOT NULL,
+  `textNote` text NOT NULL,
   `dateNote` datetime NOT NULL,
-  `idZone` int(11) NOT NULL,
+  `idArea` int(11) NOT NULL,
   PRIMARY KEY (`idNote`),
-  KEY `idZone` (`idZone`)
+  KEY `idZone` (`idArea`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -46,23 +59,10 @@ CREATE TABLE IF NOT EXISTS `point` (
   `idPoint` int(11) NOT NULL,
   `longitude` decimal(10,0) NOT NULL,
   `latitude` decimal(10,0) NOT NULL,
-  `idZone` int(11) NOT NULL,
+  `idArea` int(11) NOT NULL,
   PRIMARY KEY (`idPoint`),
-  KEY `idZone` (`idZone`)
+  KEY `idZone` (`idArea`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `zone`
---
-
-CREATE TABLE IF NOT EXISTS `zone` (
-  `idZone` int(11) NOT NULL AUTO_INCREMENT,
-  `nomZone` varchar(45) NOT NULL,
-  `couleurZone` varchar(45) NOT NULL,
-  PRIMARY KEY (`idZone`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Contraintes pour les tables exportées
@@ -72,13 +72,13 @@ CREATE TABLE IF NOT EXISTS `zone` (
 -- Contraintes pour la table `note`
 --
 ALTER TABLE `note`
-  ADD CONSTRAINT `note_ibfk_1` FOREIGN KEY (`idZone`) REFERENCES `zone` (`idZone`);
+  ADD CONSTRAINT `note_ibfk_1` FOREIGN KEY (`idArea`) REFERENCES `area` (`idArea`);
 
 --
 -- Contraintes pour la table `point`
 --
 ALTER TABLE `point`
-  ADD CONSTRAINT `point_ibfk_1` FOREIGN KEY (`idZone`) REFERENCES `zone` (`idZone`);
+  ADD CONSTRAINT `point_ibfk_1` FOREIGN KEY (`idArea`) REFERENCES `area` (`idArea`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
