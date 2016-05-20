9416fd82fa0f85575a3ea073b5c5bc4b2d4ca9ed
emilien <emilien.vilaine@gmail.com>
2016-05-20 08:27:12 +0200

    Ios 20/05 1

--- ios-client/Exametrics/Exametrics/HomeViewController.swift
+++ ios-client/Exametrics/Exametrics/HomeViewController.swift
@@ -8,15 +8,44 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource {
 
     // Variables
+    var mArea : Area!
     var noteList = [Note]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mArea = Area(id: 1, name: "The Waludo", color: "cd13b8")
+        let note1 = Note(id: 0, login: "Admin", message: "Premier test", date: NSDate(), area: mArea)
+        let note2 = Note(id: 1, login: "Admin", message: "Second test", date: NSDate(), area: mArea)
+        noteList.append(note1)
+        noteList.append(note2)
 
+        
+    }
+    
+    
+    // Fonctions du TableView
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return noteList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // Déclaration de la cellule
+        let cell = tableView.dequeueReusableCellWithIdentifier(NoteTableViewCell.identifier, forIndexPath: indexPath) as! NoteTableViewCell
+        
+        // Configuration de la cellule
+        let login = noteList[indexPath.row].getLogin()
+        let message  = noteList[indexPath.row].getMessage()
+        let date  = noteList[indexPath.row].getDate()
+
+        cell.configureWithData(login, message: message, date: date)
+        
+        return cell
         
     }
 
