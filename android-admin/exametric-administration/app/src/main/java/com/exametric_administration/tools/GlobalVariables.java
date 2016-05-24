caa1de61d7b5dd34adbb843f9519473dd35ad738
BOUCHER Clément <clement.boucherpro@gmail.com>
2016-05-24 08:39:27 +0200

    a

--- android-admin/exametric-administration/app/src/main/java/com/exametric_administration/tools/GlobalVariables.java
+++ android-admin/exametric-administration/app/src/main/java/com/exametric_administration/tools/GlobalVariables.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * Created by boucherclement on 20/05/16.
  */
 public class GlobalVariables {
-    public static String BASE_URL = "http://192.168.1.14:8080/exametrics-ws";
+    public static String BASE_URL = "http://172.30.1.178:8080/exametrics-ws";
 
     public static String AREAS_URI = "/areas";
     public static String BY_ID_AREAS_URI = "/areas?id=";
