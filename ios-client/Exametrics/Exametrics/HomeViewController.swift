a483731a1840fa4748f4b88702f32fd0a2ad19fc
emilien <emilien.vilaine@gmail.com>
2016-05-26 12:18:08 +0200

    iOS Map Localisation

--- ios-client/Exametrics/Exametrics/HomeViewController.swift
+++ ios-client/Exametrics/Exametrics/HomeViewController.swift
@@ -10,34 +10,25 @@
 
 import UIKit
 import Realm
+import CoreLocation
+import MapKit
 
-class HomeViewController: UIViewController, UITableViewDataSource {
+class HomeViewController: UIViewController, UITableViewDataSource,CLLocationManagerDelegate {
 
     // Variables
     var mArea : Area!
-    //var areaList  = [Area]()
-    //var noteList  = [Note]()
-    //var pointList = [Point]()
+    var pointList = [Point]()
+    var noteList = [Note]()
+    var areaList: RLMResults!
+    //var pointList: RLMResults!
+    //var noteList: RLMResults!
     let areaCont  = AreaController()
     let pointCont = PointController()
     let noteCont  = NoteController()
+    var locationManager: CLLocationManager!
+    var myLatitude : CLLocationDegrees!
+    var myLongitude : CLLocationDegrees!
     
-    var areaList: RLMResults {
-        get {
-            return Area.allObjects()
-        }
-    }
-    
-    var pointList: RLMResults {
-        get {
-            return Point.allObjects()
-        }
-    }
-    var noteList: RLMResults {
-        get {
-            return Note.allObjects()
-        }
-    }
     
     // Outlets
     @IBOutlet weak var noteTableView: UITableView!
@@ -47,28 +38,114 @@ class HomeViewController: UIViewController, UITableViewDataSource {
         
         
         
-        if (areaList.count == 0){
-            areaCont.getAreas()
+        //areaCont.getAreas()
+        
+        //pointCont.getPoints()
+        
+
+        
+        let point1 = Point(id: "1",longitude: 2.84692917, latitude: 42.67417863, idArea: "87")
+        let point2 = Point(id: "1",longitude: 2.84581672, latitude: 42.67432826, idArea: "87")
+        let point3 = Point(id: "1",longitude: 2.84641989, latitude: 42.67505938, idArea: "87")
+        let point4 = Point(id: "1",longitude: 2.84797993, latitude: 42.67518164, idArea: "87")
+        let point5 = Point(id: "1",longitude: 2.84828033, latitude: 42.67409975, idArea: "87")
+        pointList.append(point1)
+        pointList.append(point2)
+        pointList.append(point3)
+        pointList.append(point4)
+        pointList.append(point5)
+        
+        // Chargement des notes selon la zone actuelle
+        
+        checkLocation(pointList)
+        
+        //        
+    }
+    
+    // Localisation de la zone actuelle
+    func checkLocation(pointList: [Point]){
+        
+        
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+        // Localisation du téléphone
+        var currentLocation = CLLocation!()
+        currentLocation = locationManager.location
+        
+        myLatitude = currentLocation.coordinate.latitude
+        
+        myLongitude = currentLocation.coordinate.longitude
+        
+        var pointsCLLC = [CLLocationCoordinate2D]()
+        
+        
+        for point in pointList {
+            let locationCoor = CLLocationCoordinate2DMake(point.getLongitude(), point.getLatitude())
+            pointsCLLC.append(locationCoor)
         }
         
-        if (pointList.count == 0){
-            pointCont.getPoints()
+        let point   = MKMapPoint(x: myLongitude , y: myLatitude)
+        let polygon = MKPolygon(coordinates: &pointsCLLC, count: pointsCLLC.count)
+
+        if (isPointInPolygon(point, polygon: polygon)){
+            mArea = Area(id: "Ok", name: "YEAH", color: "Ox7cFF00FF")
+            self.title = mArea.getName()
+            
         }
 
-        if (noteList.count == 0){
-            noteCont.getNotes()
+    }
+    
+    func isPointInPolygon(point : MKMapPoint, polygon : MKPolygon) -> Bool{
+        
+        let a = polygon.points()
+        var isInsidePolygon = false
+        
+        let x = point.x
+        let y = point.y
+        
+        var i = 0
+        var j = 0
+        
+        let nvert = polygon.pointCount
+        
+        for (i = 0,  j = nvert - 1; i < nvert; j = i++) {
+            if (((a[i].y >= y) != (a[j].y >= y)) &&
+                (x <= (a[j].x - a[i].x) * (y - a[i].y) / (a[j].y - a[i].y) + a[i].x)) {
+                isInsidePolygon = !isInsidePolygon;
+            }
         }
         
-        
-        // self.title = mArea.getName()
-        noteTableView.reloadData()
-        
+        return isInsidePolygon;
+        print(isInsidePolygon)
     }
     
-    
+    /*
     func refresh() {
+        areaList = RLMResults {
+            get {
+                return Area.allObjects()
+            }
+        }
+     
+        pointList: RLMResults {
+            get {
+                return Point.allObjects()
+            }
+        }
         
+        var noteList: RLMResults {
+            get {
+                return Note.allObjects()
+            }
+        }
     }
+     */
     
     // Fonctions du TableView
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -76,10 +153,10 @@ class HomeViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
+       
         // Déclaration de la cellule
         let cell = tableView.dequeueReusableCellWithIdentifier(NoteTableViewCell.identifier, forIndexPath: indexPath) as! NoteTableViewCell
-        
+        /*
         // Récupération des notes
         let index = UInt(indexPath.row)
         let noteItem = noteList.objectAtIndex(index) as! Note // [4]
@@ -90,7 +167,7 @@ class HomeViewController: UIViewController, UITableViewDataSource {
         let date   = noteItem.getDate()
         
         cell.configureWithData(author, text: text, date: date)
-        
+        */
         return cell
         
     }
--- ios-client/Exametrics/Exametrics/MapViewController.swift
+++ ios-client/Exametrics/Exametrics/MapViewController.swift
@@ -43,13 +43,15 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         centerMapOnLocation(initialLocation)
         
         if(pointList.count == 0) {
-            let point1 = Point(id: "1",longitude: 41, latitude: 1, idArea: "87")
-            let point2 = Point(id: "1",longitude: 42, latitude: 1, idArea: "87")
-            let point3 = Point(id: "1",longitude: 41.5, latitude: 2, idArea: "87")
-            let point4 = Point(id: "1",longitude: 41, latitude: 2, idArea: "87")
+            let point1 = Point(id: "1",longitude: 2.84692917, latitude: 42.67417863, idArea: "87")
+            let point2 = Point(id: "2",longitude: 2.84581672, latitude: 42.67432826, idArea: "87")
+            let point3 = Point(id: "3",longitude: 2.84641989, latitude: 42.67505938, idArea: "87")
+            let point4 = Point(id: "4",longitude: 2.84797993, latitude: 42.67518164, idArea: "87")
+            let point5 = Point(id: "5",longitude: 2.84828033, latitude: 42.67409975, idArea: "87")
             pointList.append(point1)
             pointList.append(point2)
             pointList.append(point3)
+            
         }
         
         myArea = Area(id: "1", name: "LaZone", color: "Ox7c880088")
@@ -61,7 +63,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     func addBoundry()
     {
         var pointsCLLC = [CLLocationCoordinate2D]()
-        
         
         for point in pointList {
             let locationCoor = CLLocationCoordinate2DMake(point.getLongitude(), point.getLatitude())
@@ -102,36 +103,11 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         
         let location = locations.last! as CLLocation
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
         
         self.map.setRegion(region, animated: true)
     }
 
-    
-    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView!
-    {
-        if !(annotation is MKPointAnnotation)
-        {
-            return nil
-        }
-        
-        let reuseId = "test"
-        
-        var anView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
-        
-        if anView == nil
-        {
-            anView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-            anView!.image = UIImage(named:"pin")
-            anView!.canShowCallout = true
-        }
-        else
-        {
-            anView!.annotation = annotation
-        }
-        
-        return anView
-    }
     
     
     
