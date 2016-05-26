fef18bfdc5925f42272a16a283b4eb884aa17cbb
BOUCHER Clément <clement.boucherpro@gmail.com>
2016-05-26 17:33:21 +0200

    BDD

--- bdd/exametrics.sql
+++ bdd/exametrics.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:8889
--- GÈnÈrÈ le :  Mer 25 Mai 2016 ‡ 19:49
+-- G√©n√©r√© le :  Jeu 26 Mai 2016 √† 17:32
 -- Version du serveur :  5.5.42
 -- Version de PHP :  5.6.10
 
@@ -11,7 +11,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Base de donnÈes :  `exametrics`
+-- Base de donn√©es :  `exametrics`
 --
 
 -- --------------------------------------------------------
@@ -24,37 +24,19 @@ CREATE TABLE `area` (
   `idArea` int(11) NOT NULL,
   `nameArea` varchar(45) NOT NULL,
   `colorArea` varchar(45) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=113 DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=132 DEFAULT CHARSET=latin1;
 
 --
--- Structure de la table `note`
+-- Contenu de la table `area`
 --
 
-CREATE TABLE `note` (
-  `idNote` int(11) NOT NULL,
-  `authorNote` varchar(45) DEFAULT 'Anonyme',
-  `textNote` text NOT NULL,
-  `dateNote` datetime NOT NULL,
-  `idArea` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+INSERT INTO `area` (`idArea`, `nameArea`, `colorArea`) VALUES
+(129, 'int√©rieur', '0x7fab0000'),
+(130, 'ext√©rieur', '0x7f00a100'),
+(131, 'tressere', '0x7f1c1326');
 
 --
--- Structure de la table `point`
---
-
-CREATE TABLE `point` (
-  `idPoint` int(11) NOT NULL,
-  `longitude` decimal(12,8) NOT NULL,
-  `latitude` decimal(12,8) NOT NULL,
-  `idArea` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=249 DEFAULT CHARSET=latin1;
-
---
--- Index pour les tables exportÈes
+-- Index pour les tables export√©es
 --
 
 --
@@ -64,50 +46,11 @@ ALTER TABLE `area`
   ADD PRIMARY KEY (`idArea`);
 
 --
--- Index pour la table `note`
---
-ALTER TABLE `note`
-  ADD PRIMARY KEY (`idNote`),
-  ADD KEY `idZone` (`idArea`);
-
---
--- Index pour la table `point`
---
-ALTER TABLE `point`
-  ADD PRIMARY KEY (`idPoint`),
-  ADD KEY `idZone` (`idArea`);
-
---
--- AUTO_INCREMENT pour les tables exportÈes
+-- AUTO_INCREMENT pour les tables export√©es
 --
 
 --
 -- AUTO_INCREMENT pour la table `area`
 --
 ALTER TABLE `area`
-  MODIFY `idArea` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=113;
---
--- AUTO_INCREMENT pour la table `note`
---
-ALTER TABLE `note`
-  MODIFY `idNote` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
---
--- AUTO_INCREMENT pour la table `point`
---
-ALTER TABLE `point`
-  MODIFY `idPoint` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=249;
---
--- Contraintes pour les tables exportÈes
---
-
---
--- Contraintes pour la table `note`
---
-ALTER TABLE `note`
-  ADD CONSTRAINT `fk_note_id_area` FOREIGN KEY (`idArea`) REFERENCES `area` (`idArea`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `point`
---
-ALTER TABLE `point`
-  ADD CONSTRAINT `fk_point_id_area` FOREIGN KEY (`idArea`) REFERENCES `area` (`idArea`) ON DELETE CASCADE;
+  MODIFY `idArea` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=132;
